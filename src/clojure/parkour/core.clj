bbf554a94fc59b36ee4adeea65522199a49f4794
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-07-12 10:35:50 -0400

    More ways to reduce MR input.

--- src/clojure/parkour/core.clj
+++ src/clojure/parkour/core.clj
@@ -1,20 +1,9 @@
 (ns parkour.core
   (:require [clojure.core.reducers :as r]
             [clojure.core.protocols :as ccp]
-            [clojure.string :as str]
-            [cheshire.core :as json])
-  (:import [org.apache.hadoop.io Text NullWritable]
-           [org.apache.hadoop.mapreduce ReduceContext TaskInputOutputContext]
-           [org.apache.hadoop.mapreduce.lib.output MultipleOutputs]
-           [org.apache.avro Schema Schema$Parser]
-           [org.apache.avro.generic GenericData GenericData$Record]))
-
-(set! *warn-on-reflection* true)
-
-(defmacro returning
-  "Evaluates the result of expr, then evaluates the forms in body (presumably
-for side-effects), then returns the result of expr."
-  [expr & body] `(let [value# ~expr] ~@body value#))
+            [clojure.string :as str])
+  (:import [org.apache.hadoop.mapreduce ReduceContext TaskInputOutputContext]
+           [org.apache.hadoop.mapreduce.lib.output MultipleOutputs]))
 
 (defmacro ^:private context-reducer
   [nextf dataf]
@@ -32,13 +21,31 @@ for side-effects), then returns the result of expr."
                    [(.getCurrentKey context)
                     (.getCurrentValue context)]))
 
+(defn mr-keys
+  [^TaskInputOutputContext context]
+  (context-reducer (.nextKeyValue context)
+                   (.getCurrentKey context)))
+
+(defn mr-vals
+  [^TaskInputOutputContext context]
+  (context-reducer (.nextKeyValue context)
+                   (.getCurrentValue context)))
+
 (defn mr-keyvalgroups
   [^ReduceContext context]
   (context-reducer (.nextKey context)
                    [(.getCurrentKey context)
                     (.getValues context)]))
 
+(defn mr-keygroups
+  [^ReduceContext context]
+  (context-reducer (.nextKey context)
+                   (.getCurrentKey context)))
+
+(defn mr-valgroups
+  [^ReduceContext context]
+  (context-reducer (.nextKey context) (.getValues context)))
+
 (defn emit-keyvals
   [^TaskInputOutputContext context coll]
   (r/reduce (fn [_ [k v]] (.write context k v)) nil coll))
-
