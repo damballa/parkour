f3aeb6f0bce80715daeebf0ea607fa0f1882163b
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-07-12 10:34:44 -0400

    Narrow scope -> prune dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :warn-on-reflection true
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
-                 [org.apache.avro/avro "1.7.4"]
-                 [cheshire "5.1.1"]]
+                 [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]
   :profiles {:provided
              {:dependencies
               [[org.apache.hadoop/hadoop-core "1.0.3"]]}})
