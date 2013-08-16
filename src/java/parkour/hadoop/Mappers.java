1a4b311a39c95f5e7eac4ebc75e00c3f10b4dcbf
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-08-16 10:33:23 -0400

    Switch to more typical Java indentation.

--- src/java/parkour/hadoop/Mappers.java
+++ src/java/parkour/hadoop/Mappers.java
@@ -4,27 +4,25 @@ import clojure.lang.IFn;
 import clojure.lang.RT;
 import clojure.lang.Symbol;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class Mappers {
+  private static class Base extends Mapper {
+    private static final String CONF_KEY_BASE = "parkour.mapper.";
 
-private static class Base extends org.apache.hadoop.mapreduce.Mapper {
-  private static final String CONF_KEY_BASE = "parkour.mapper.";
+    private final String varKey;
+    private final String argsKey;
+    private final long id;
 
-  private final String varKey;
-  private final String argsKey;
-  private final long id;
-
-  public
-  Base() {
+    public Base() {
       super();
       this.id = Long.parseLong(getClass().getName().split("\\$_", 2)[1]);
       String confKey = CONF_KEY_BASE + Long.toString(this.id);
       this.varKey = confKey + ".var";
       this.argsKey = confKey + ".args";
-  }
+    }
 
-  public void
-  run(Context context) {
+    public void run(Context context) {
       Configuration conf = context.getConfiguration();
       String[] fqname = conf.get(varKey).split("/", 2);
       if (fqname[0].startsWith("#'")) fqname[0] = fqname[0].substring(2);
@@ -32,21 +30,20 @@ private static class Base extends org.apache.hadoop.mapreduce.Mapper {
       IFn tvar = RT.var(fqname[0], fqname[1]);
       String argsEDN = conf.get(argsKey);
       IFn f = (argsEDN == null)
-          ? (IFn) tvar.invoke(conf)
-          : (IFn) tvar.applyTo(RT.cons(conf, RT.readString(argsEDN)));
+        ? (IFn) tvar.invoke(conf)
+        : (IFn) tvar.applyTo(RT.cons(conf, RT.readString(argsEDN)));
       f.invoke(context, context);
+    }
   }
-}
 
-public static class _0 extends Base { }
-public static class _1 extends Base { }
-public static class _2 extends Base { }
-public static class _3 extends Base { }
-public static class _5 extends Base { }
-public static class _4 extends Base { }
-public static class _6 extends Base { }
-public static class _7 extends Base { }
-public static class _8 extends Base { }
-public static class _9 extends Base { }
-
+  public static class _0 extends Base { }
+  public static class _1 extends Base { }
+  public static class _2 extends Base { }
+  public static class _3 extends Base { }
+  public static class _5 extends Base { }
+  public static class _4 extends Base { }
+  public static class _6 extends Base { }
+  public static class _7 extends Base { }
+  public static class _8 extends Base { }
+  public static class _9 extends Base { }
 }
--- src/java/parkour/hadoop/Partitioner.java
+++ src/java/parkour/hadoop/Partitioner.java
@@ -8,22 +8,20 @@ import org.apache.hadoop.conf.Configuration;
 
 
 public class Partitioner
-    extends org.apache.hadoop.mapreduce.Partitioner
-    implements Configurable {
+  extends org.apache.hadoop.mapreduce.Partitioner
+  implements Configurable {
 
-private static final String VAR_KEY = "parkour.partitioner.var";
-private static final String ARGS_KEY = "parkour.partitioner.args";
+  private static final String VAR_KEY = "parkour.partitioner.var";
+  private static final String ARGS_KEY = "parkour.partitioner.args";
 
-private Configuration conf = null;
-private IFn f = null;
+  private Configuration conf = null;
+  private IFn f = null;
 
-public Configuration
-getConf() {
+  public Configuration getConf() {
     return this.conf;
-}
+  }
 
-public void
-setConf(Configuration conf) {
+  public void setConf(Configuration conf) {
     this.conf = conf;
     String[] fqname = conf.get(VAR_KEY).split("/", 2);
     if (fqname[0].startsWith("#'")) fqname[0] = fqname[0].substring(2);
@@ -31,16 +29,14 @@ setConf(Configuration conf) {
     IFn tvar = RT.var(fqname[0], fqname[1]);
     String argsEDN = conf.get(ARGS_KEY);
     this.f = (argsEDN == null)
-        ? (IFn) tvar.invoke(conf)
-        : (IFn) tvar.applyTo(RT.cons(conf, RT.readString(argsEDN)));
-}
+      ? (IFn) tvar.invoke(conf)
+      : (IFn) tvar.applyTo(RT.cons(conf, RT.readString(argsEDN)));
+  }
 
-@Override
-public int
-getPartition(Object key, Object val, int numPartitions) {
+  @Override
+  public int getPartition(Object key, Object val, int numPartitions) {
     if (f instanceof IFn.OOLL)
-        return (int) ((IFn.OOLL) f).invokePrim(key, val, numPartitions);
+      return (int) ((IFn.OOLL) f).invokePrim(key, val, numPartitions);
     return RT.intCast(f.invoke(key, val, numPartitions));
-}
-
+  }
 }
