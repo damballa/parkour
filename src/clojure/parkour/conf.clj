d64f9114f0d25e0b0ade98447d435a37c1faa3fd
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-08-31 09:57:50 -0400

    Further refine configuration API.

--- src/clojure/parkour/conf.clj
+++ src/clojure/parkour/conf.clj
@@ -1,44 +1,20 @@
 (ns parkour.conf
-  (:refer-clojure :exclude [assoc!])
+  (:refer-clojure :exclude [assoc! get])
   (:require [clojure.string :as str]
+            [parkour.wrapper :as w]
             [parkour.util :refer [returning]])
   (:import [java.util List Map Set]
            [org.apache.hadoop.conf Configuration]
-           [org.apache.hadoop.mapred JobConf]))
+           [org.apache.hadoop.mapreduce Job]))
 
 (defmulti ^:private conf-set*
   "Internal implementation multimethod for setting value in Hadoop config."
   (fn [_ _ val] (type val)))
 
-(defmacro ^:private def-conf-set*
-  [[conf key val] & pairs]
-  (let [conf (vary-meta conf assoc :tag `Configuration)
-        key (vary-meta key assoc :tag `String)]
-    `(do ~@(map (fn [[type form]]
-                  (let [val (vary-meta val assoc :tag type)]
-                    `(defmethod conf-set* ~type ~[conf key val] ~form)))
-                (partition 2 pairs)))))
-
-(def-conf-set* [conf key val]
-  String  (.set conf key val)
-  Integer (.setInt conf key (int val))
-  Long    (.setLong conf key (long val))
-  Float   (.setFloat conf key (float val))
-  Double  (.setFloat conf key (float val))
-  Boolean (.setBoolean conf key (boolean val)))
-
 (defmulti ^:private conf-coerce
   "Internal implementation multimethod for converting value to one
 which may be set as Hadoop config value."
   type)
-
-(defmethod conf-coerce :default [val] val)
-(defmethod conf-coerce Class [val] (.getName ^Class val))
-(defmethod conf-coerce List [val] (str/join "," (map conf-coerce val)))
-(defmethod conf-coerce Set [val] (str/join "," (map conf-coerce val)))
-(defmethod conf-coerce Map [val]
-  (let [kv-str (fn [[k v]] (str (conf-coerce k) "=" (conf-coerce v)))]
-    (str/join "," (map kv-str val))))
 
 (defn set!
   "Set `conf` parameter `key` to `val`."
@@ -56,17 +32,45 @@ which may be set as Hadoop config value."
   "Merge key-value pairs `kvs` into Hadoop configuration `conf`."
   [conf & kvs] (merge! conf (partition 2 kvs)))
 
-(defmacro ^:private make-conf-fn
-  [name class]
-  `(defn ~name
-     ~(str "Create a new " class ".")
-     ([] (new ~class))
-     ([~'conf-map]
-        (if (map? ~'conf-map)
-          (doto (~name) (merge! ~'conf-map))
-          (new ~class ~(with-meta 'conf-map {:tag class}))))
-     ([~'conf ~'conf-map]
-        (merge! ~'conf ~'conf-map))))
+(defn configuration
+  "Produce a Hadoop `Configuration`.  If provided a `conf` argument,
+coerce it to be a `Configuration` which if possible shares mutable
+state with that original argument."
+  {:tag `Configuration}
+  ([] (Configuration.))
+  ([conf]
+     (condp instance? conf
+       Configuration conf
+       Job (.getConfiguration ^Job conf)
+       Map (merge! (configuration) conf))))
 
-(make-conf-fn job-conf JobConf)
-(make-conf-fn conf Configuration)
+(def ^{:tag `Configuration} iguration
+  "Alias for `configuration`."
+  configuration)
+
+(defmacro ^:private def-conf-set*
+  [[conf key val] & pairs]
+  (let [conf (vary-meta conf assoc :tag `Configuration)
+        key (vary-meta key assoc :tag `String)]
+    `(do ~@(map (fn [[type form]]
+                  (let [val (vary-meta val assoc :tag type)]
+                    `(defmethod conf-set* ~type ~[conf key val]
+                       (let [~conf (w/unwrap ~conf)]
+                         ~form))))
+                (partition 2 pairs)))))
+
+(def-conf-set* [conf key val]
+  String  (.set conf key val)
+  Integer (.setInt conf key (int val))
+  Long    (.setLong conf key (long val))
+  Float   (.setFloat conf key (float val))
+  Double  (.setFloat conf key (float val))
+  Boolean (.setBoolean conf key (boolean val)))
+
+(defmethod conf-coerce :default [val] val)
+(defmethod conf-coerce Class [val] (.getName ^Class val))
+(defmethod conf-coerce List [val] (str/join "," (map conf-coerce val)))
+(defmethod conf-coerce Set [val] (str/join "," (map conf-coerce val)))
+(defmethod conf-coerce Map [val]
+  (let [kv-str (fn [[k v]] (str (conf-coerce k) "=" (conf-coerce v)))]
+    (str/join "," (map kv-str val))))
