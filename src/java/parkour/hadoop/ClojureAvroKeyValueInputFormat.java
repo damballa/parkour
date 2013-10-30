5ec9aa2d4c255addc3b5dd6b75c3d5c68e479059
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-10-30 13:15:40 -0400

    Invoke via Clojure to avoid static interface vs. class method call.

--- src/java/parkour/hadoop/ClojureAvroKeyValueInputFormat.java
+++ src/java/parkour/hadoop/ClojureAvroKeyValueInputFormat.java
@@ -2,6 +2,10 @@ package parkour.hadoop;
 
 import java.io.IOException;
 
+import clojure.lang.RT;
+import clojure.lang.Symbol;
+import clojure.lang.Var;
+
 import org.apache.avro.Schema;
 import org.apache.avro.mapred.AvroKey;
 import org.apache.avro.mapred.AvroValue;
@@ -14,13 +18,22 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
 public class ClojureAvroKeyValueInputFormat<K, V>
     extends FileInputFormat<AvroKey<K>, AvroValue<V>> {
+  private static class Vars {
+    private static final String NS = "parkour.conf";
+    private static final Var configuration = RT.var(NS, "configuration");
+    static {
+      RT.var("clojure.core", "require").invoke(Symbol.intern(NS));
+    }
+  }
 
   @Override
   public RecordReader<AvroKey<K>, AvroValue<V>>
     createRecordReader(InputSplit split, TaskAttemptContext context)
       throws IOException, InterruptedException {
 
-    Configuration conf = context.getConfiguration();
+    // Get `Configuration` via `parkour.conf/ig` to avoid caring at compile-time
+    // if `TaskAttempContext` is an interface or a class.
+    Configuration conf = (Configuration) Vars.configuration.invoke(context);
     Schema ks = AvroJob.getInputKeySchema(conf);
     Schema vs = AvroJob.getInputValueSchema(conf);
     return new ClojureAvroKeyValueRecordReader<K, V>(ks, ks);
