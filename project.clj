7818c1859d88918af2b03b95aba686e34b563c92
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-11-02 15:09:58 -0400

    Begin work on next iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.damballa/parkour "0.3.3"
+(defproject com.damballa/parkour "0.3.4-SNAPSHOT"
   :description "Hadoop MapReduce in idiomatic Clojure."
   :url "http://github.com/damballa/parkour"
   :license {:name "Eclipse Public License"
