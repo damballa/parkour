d54ffdd065a753a1c8c6e8d8292c3fb695d61c93
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-11-04 05:56:18 -0500

    Prepare build for public release.

--- project.clj
+++ project.clj
@@ -27,23 +27,32 @@
                     parkour.remote.mux
                     parkour.util]
           :output-dir "tmp/codox"}
-  :aliases {"test-all" ["with-profile" ~(str "default,hadoop-0-20-205:"
-                                             "default,hadoop-1-0-3:"
-                                             "default,hadoop-1-2-1:"
-                                             "default,hadoop-cdh3:"
-                                             "default,hadoop-cdh4:"
-                                             "default,hadoop-2-2-0")
+  :aliases {"test-all" ["with-profile" ~(str "+hadoop-0-20-205:"
+                                             "+hadoop-1-0-3:"
+                                             "+hadoop-1-2-1:"
+                                             "+hadoop-cdh3:"
+                                             "+hadoop-cdh4:"
+                                             "+hadoop-2-2-0")
                         ,              "test"]}
-  :profiles {:provided {:dependencies
-                        [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
-                         [org.apache.hadoop/hadoop-core "1.2.1"]
+  :profiles {:default [:base :system :user :provided :java6 :dev]
+             :conjars {:repositories
+                       [["conjars" "http://conjars.org/repo/"]]}
+             :cloudera {:repositories
+                        [["cloudera" ~(str "https://repository.cloudera.com"
+                                           "/artifactory/cloudera-repos/")]]}
+             :platypope {:repositories
+                         [["platypope" "http://jars.platypope.org/release/"]]}
+             :provided {:dependencies
+                        [[org.apache.hadoop/hadoop-core "1.2.1"]
                          [org.slf4j/slf4j-api "1.6.1"]
                          [org.slf4j/slf4j-log4j12 "1.6.1"]
                          [log4j "1.2.17"]]}
+             :java6 {:dependencies
+                     [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}
              :examples {:source-paths ["examples"]}
              :dev [:examples
-                   {:repositories {"conjars" "http://conjars.org/repo/"}
-                    :dependencies
+                   :conjars ;; For cascading-* *
+                   {:dependencies
                     [[cascading/cascading-hadoop "2.2.0"
                       :exclusions [org.codehaus.janino/janino
                                    org.apache.hadoop/hadoop-core]]]}]
@@ -54,21 +63,17 @@
                             [[org.apache.hadoop/hadoop-core "1.0.3"]]}
              :hadoop-1-2-1 {:dependencies
                             [[org.apache.hadoop/hadoop-core "1.2.1"]]}
-             :hadoop-cdh3 {:repositories
-                           [["cloudera" ~(str "https://repository.cloudera.com"
-                                              "/artifactory/cloudera-repos/")]]
-                           :dependencies
-                           [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u3"]]}
-             :hadoop-cdh4 {:repositories
-                           [["cloudera" ~(str "https://repository.cloudera.com"
-                                              "/artifactory/cloudera-repos/")]
-                            ["damballa" "http://jars.repo.rnd.atl.damballa/"]]
-                           :dependencies
-                           [[org.apache.hadoop/hadoop-core "2.0.0-mr1-cdh4.1.2"]
-                            [org.apache.hadoop/hadoop-common "2.0.0-cdh4.1.2"]
-                            [org.apache.avro/avro-mapred "1.7.5"
-                             :classifier "hadoop1-cdh4"
-                             :exclusions [org.apache.avro/avro-ipc]]]}
+             :hadoop-cdh3 [:cloudera ;; For hadoop-* *-*cdh3*
+                           {:dependencies
+                            [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u3"]]}]
+             :hadoop-cdh4 [:cloudera ;; For hadoop-* *-*cdh4*
+                           :platypope ;; For avro-mapred 1.7.5 hadoop1-cdh4
+                           {:dependencies
+                            [[org.apache.hadoop/hadoop-core "2.0.0-mr1-cdh4.1.2"]
+                             [org.apache.hadoop/hadoop-common "2.0.0-cdh4.1.2"]
+                             [org.apache.avro/avro-mapred "1.7.5"
+                              :classifier "hadoop1-cdh4"
+                              :exclusions [org.apache.avro/avro-ipc]]]}]
              :hadoop-2-2-0 {:dependencies
                             [[org.apache.hadoop/hadoop-client "2.2.0"]
                              [org.apache.hadoop/hadoop-common "2.2.0"]
