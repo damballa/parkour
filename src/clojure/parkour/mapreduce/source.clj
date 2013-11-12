f2a1132194696e49f7fd517a11ee34996619bd6c
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-11-12 17:09:07 -0500

    Support task tuple collections as lazy sequences.

--- src/clojure/parkour/mapreduce/source.clj
+++ src/clojure/parkour/mapreduce/source.clj
@@ -1,10 +1,11 @@
 (ns parkour.mapreduce.source
-  (:refer-clojure :exclude [key val keys vals reduce])
+  (:refer-clojure :exclude [key val keys vals])
   (:require [clojure.core :as cc]
             [clojure.core.reducers :as r]
             [clojure.core.protocols :as ccp]
             [parkour (conf :as conf) (wrapper :as w)])
-  (:import [org.apache.hadoop.conf Configurable]
+  (:import [clojure.lang Seqable]
+           [org.apache.hadoop.conf Configurable]
            [org.apache.hadoop.mapreduce MapContext ReduceContext]
            [org.apache.hadoop.mapreduce TaskInputOutputContext]))
 
@@ -30,37 +31,65 @@ of such tuples."
   "Pair of current tuple's key and sequence of associated values."
   [context] [(key context) (vals context)])
 
+(defn mapping
+  "A `seq`able or `reduce`able mapping of `f` for each item in `coll`."
+  [f coll]
+  (reify
+    ccp/CollReduce
+    (coll-reduce [this f1] (ccp/coll-reduce this f1 (f1)))
+    (coll-reduce [_ f1 init] (r/reduce f1 init (r/map f coll)))
+
+    Seqable
+    (seq [_] (map f coll))))
+
 (defn keykeyvals
   "Pair of current tuple's key and sequence of grouped key and value pairs."
-  [context] [(key context) (r/map #(-> [(key context) %]) (vals context))])
+  [context] [(key context) (mapping #(-> [(key context) %]) (vals context))])
 
 (defn keykeys
   "Pair of current tuple's key and sequence of grouped keys."
-  [context] [(key context) (r/map (fn [_] (key context)) (vals context))])
+  [context] [(key context) (mapping (fn [_] (key context)) (vals context))])
 
 (defn keys
   "Current grouping key's sequence of grouped keys."
-  [context] (r/map (fn [_] (key context)) (vals context)))
+  [context] (mapping (fn [_] (key context)) (vals context)))
 
-(defn reduce
-  "As per `cc/reduce`, but in terms of the `TupleSource` protocol.  When
-provided, applies `nextf` to `context` to retrieve the next tuple source for
-each iteration and `dataf` to retrieve the tuple values passed to `f`."
-  ([context f init]
-     (reduce next-keyval keyval context f init))
-  ([nextf dataf context f init]
-     (loop [context context, state init]
-       (if-let [context (nextf context)]
-         (recur context (f state (dataf context)))
+(defn source-reduce
+  "As per `reduce`, but in terms of the `TupleSource` protocol.  When provided,
+applies `nextf` to `source` to retrieve the next tuple source for each iteration
+and `dataf` to retrieve the tuple values passed to `f`."
+  ([source f init]
+     (source-reduce next-keyval keyval source f init))
+  ([nextf dataf source f init]
+     (loop [source source, state init]
+       (if-let [source (nextf source)]
+         (recur source (f state (dataf source)))
          state))))
 
+(defn source-seq
+  "A seq for `source`, in terms of the `TupleSource` protocol.  When provided,
+applies `nextf` to `source` to retrieve the next tuple source for each iteration
+and `dataf` to retrieve the tuple values passed to `f`."
+  ([source]
+     (source-seq next-keyval keyval source))
+  ([nextf dataf source]
+     ((fn step [source]
+        (lazy-seq
+         (if-let [source (nextf source)]
+           (cons (dataf source) (step source)))))
+      source)))
+
 (defn reducer
-  "Make a tuple source `source` `cc/reduce`able with particular
+  "Make a tuple source `source` `reduce`able and `seq`able with particular
 iteration function `nextf` and extraction function `dataf`."
   [nextf dataf source]
-  (reify ccp/CollReduce
+  (reify
+    ccp/CollReduce
     (coll-reduce [this f] (ccp/coll-reduce this f (f)))
-    (coll-reduce [this f init] (reduce nextf dataf source f init))))
+    (coll-reduce [_ f init] (source-reduce nextf dataf source f init))
+
+    Seqable
+    (seq [_] (source-seq nextf dataf source))))
 
 (extend-protocol TupleSource
   MapContext
@@ -79,7 +108,7 @@ iteration function `nextf` and extraction function `dataf`."
   TaskInputOutputContext
   (coll-reduce
     ([this f] (ccp/coll-reduce this f (f)))
-    ([this f init] (reduce this f init))))
+    ([this f init] (source-reduce this f init))))
 
 (extend-protocol w/Wrapper
   TaskInputOutputContext
@@ -91,10 +120,13 @@ iteration function `nextf` and extraction function `dataf`."
       TupleSource
       (key [_] (w/unwrap (key wobj)))
       (val [_] (w/unwrap (val wobj)))
-      (vals [_] (r/map w/unwrap (vals wobj)))
+      (vals [_] (mapping w/unwrap (vals wobj)))
       (next-keyval [this] (when (next-keyval wobj) this))
       (next-key [this] (when (next-key wobj) this))
 
       ccp/CollReduce
       (coll-reduce [this f] (ccp/coll-reduce this f (f)))
-      (coll-reduce [this f init] (reduce this f init)))))
+      (coll-reduce [this f init] (source-reduce this f init))
+
+      Seqable
+      (seq [this] (source-seq this)))))
